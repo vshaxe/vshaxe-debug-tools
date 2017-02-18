049cfd507bd906dbc608f0ae066b834bebac0f74
Gama11 <jensfischer95@gmail.com>
2017-02-18 23:44:15 +0100

    Add a "Generate Sample HTML" task
    Useful to play around with the DOM in the browser.

---
+++ test/Test.hx
@@ -0,0 +1,14 @@
+import sys.io.File;
+
+class Test {
+    public static function main() {
+        var src = File.getContent("src/Vis.hx");
+        HxParser.parse("hxparser", src, function(data) switch(data) {
+            case Success(data):
+                var parsed = JsonParser.parse(data);
+                var html = Vis.vis("", parsed, 0);
+                File.saveContent("TestPage.html", html);
+            case _:
+        });
+    }
+}
