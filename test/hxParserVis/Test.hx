29dd25b5287380a8717b3547c907ab875bf1785b
Gama11 <jensfischer95@gmail.com>
2017-02-24 23:12:02 +0100

    Fix inconsistent font in TestPage.html

--- test/hxParserVis/Test.hx
+++ test/hxParserVis/Test.hx
@@ -13,7 +13,7 @@ class Test {
              case Success(data):
                 var parsed = JsonParser.parse(data);
                 var html = HtmlPrinter.print("", data, parsed, 0, SyntaxTree);
-                html = html.replace("<body>", "<body style='background-color: rgb(30, 30, 30); font-family: Consolas; font-size: 12'>");
+                html = html.replace("<body>", "<body style='background-color: rgb(30, 30, 30); font-family: \"Segoe UI\"; font-size: 13; color: white;'>");
                 File.saveContent("bin/TestPage.html", html);
             case _:
                 Sys.exit(1);
