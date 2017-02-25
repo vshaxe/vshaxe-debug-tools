f837b1f5f3c1a987c5d697686323eb1ff8bbda37
Gama11 <jensfischer95@gmail.com>
2017-02-25 22:01:40 +0100

    import vscode.*;

--- src/hxParserVis/Main.hx
+++ src/hxParserVis/Main.hx
@@ -1,16 +1,18 @@
 package hxParserVis;
 
+import Vscode.*;
 import js.node.Buffer;
+import vscode.*;
 
 class Main {
-    var context:vscode.ExtensionContext;
+    var context:ExtensionContext;
 
-    function new(context:vscode.ExtensionContext) {
+    function new(context:ExtensionContext) {
         this.context = context;
 
         var provider = new ContentProvider();
 
-        var highlightDecoration = Vscode.window.createTextEditorDecorationType({
+        var highlightDecoration = window.createTextEditorDecorationType({
             borderWidth: '1px',
             borderStyle: 'solid',
             borderColor: 'rgba(255,255,0,0.3)',
@@ -18,33 +20,33 @@ class Main {
         });
         context.subscriptions.push(highlightDecoration);
 
-        context.subscriptions.push(Vscode.workspace.registerTextDocumentContentProvider('hxparservis', provider));
+        context.subscriptions.push(workspace.registerTextDocumentContentProvider('hxparservis', provider));
 
-        context.subscriptions.push(Vscode.window.onDidChangeActiveTextEditor(function(editor) {
+        context.subscriptions.push(window.onDidChangeActiveTextEditor(function(editor) {
             provider.updateText();
         }));
 
         // TODO: figure out what to do with regular updates as we can now receive the language server's incremental parsing results
-        /*context.subscriptions.push(Vscode.workspace.onDidChangeTextDocument(function(e) {
-            var activeEditor = Vscode.window.activeTextEditor;
+        /*context.subscriptions.push(workspace.onDidChangeTextDocument(function(e) {
+            var activeEditor = window.activeTextEditor;
             if (activeEditor != null && e.document == activeEditor.document) {
                 activeTextEditor.setDecorations(highlightDecoration, []);
                 provider.updateText();
             }
         }));*/
 
-        context.subscriptions.push(Vscode.commands.registerCommand("hxparservis.updateParseTree", function(uri:String, parseTree:String) {
+        context.subscriptions.push(commands.registerCommand("hxparservis.updateParseTree", function(uri:String, parseTree:String) {
             if (uri == provider.previousEditor.document.uri.toString())
                 provider.updateText(haxe.Unserializer.run(parseTree));
         }));
 
-        context.subscriptions.push(Vscode.window.onDidChangeTextEditorSelection(function(e) {
-            if (e.textEditor == Vscode.window.activeTextEditor) {
+        context.subscriptions.push(window.onDidChangeTextEditorSelection(function(e) {
+            if (e.textEditor == window.activeTextEditor) {
                 provider.highlightNode(e.textEditor.document.offsetAt(e.textEditor.selection.anchor));
             }
         }));
 
-        context.subscriptions.push(Vscode.workspace.onDidCloseTextDocument(function(e) {
+        context.subscriptions.push(workspace.onDidCloseTextDocument(function(e) {
             if (e.fileName != "\\hxparservis")
                 return;
             forEditorWithUri(provider.previousEditor.document.uri.toString(), function(editor) {
@@ -52,28 +54,28 @@ class Main {
             });
         }));
 
-        context.subscriptions.push(Vscode.commands.registerCommand("hxparservis.visualize", function() {
-            return Vscode.commands.executeCommand('vscode.previewHtml', ContentProvider.visUri, vscode.ViewColumn.Two, 'hxparser visualization')
-                .then(null, function(error) Vscode.window.showErrorMessage(error));
+        context.subscriptions.push(commands.registerCommand("hxparservis.visualize", function() {
+            return commands.executeCommand('vscode.previewHtml', ContentProvider.visUri, ViewColumn.Two, 'hxparser visualization')
+                .then(null, function(error) window.showErrorMessage(error));
         }));
 
-        context.subscriptions.push(Vscode.commands.registerCommand("hxparservis.reveal", function(uri:String, start:Int, end:Int) {
+        context.subscriptions.push(commands.registerCommand("hxparservis.reveal", function(uri:String, start:Int, end:Int) {
             forEditorWithUri(uri, function(editor) {
-                var range = new vscode.Range(editor.document.positionAt(start), editor.document.positionAt(end));
+                var range = new Range(editor.document.positionAt(start), editor.document.positionAt(end));
                 editor.revealRange(range, InCenter);
                 editor.setDecorations(highlightDecoration, [range]);
             });
         }));
 
-        context.subscriptions.push(Vscode.commands.registerCommand("hxparservis.switchOutput", function(outputKind:String) {
+        context.subscriptions.push(commands.registerCommand("hxparservis.switchOutput", function(outputKind:String) {
             provider.switchOutputKind(outputKind);
         }));
 
         createCursorOffsetStatusBarItem();
     }
 
-    function forEditorWithUri(uri:String, callback:vscode.TextEditor->Void) {
-        for (editor in Vscode.window.visibleTextEditors) {
+    function forEditorWithUri(uri:String, callback:TextEditor->Void) {
+        for (editor in window.visibleTextEditors) {
             if (editor.document.uri.toString() == uri)
                 callback(editor);
         }
@@ -81,29 +83,29 @@ class Main {
 
     /** Useful for debugging Haxe display requests, since the cursor offset is needed there. **/
     function createCursorOffsetStatusBarItem() {
-        var cursorOffset = Vscode.window.createStatusBarItem(Right, 100);
+        var cursorOffset = window.createStatusBarItem(Right, 100);
         cursorOffset.tooltip = "Cursor byte offset";
         context.subscriptions.push(cursorOffset);
 
         function updateItem() {
-            var editor = Vscode.window.activeTextEditor;
+            var editor = window.activeTextEditor;
             if (editor == null || editor.document.languageId != "haxe") {
                 cursorOffset.hide();
                 return;
             }
             var pos = editor.selection.start;
-            var textUntilCursor = editor.document.getText(new vscode.Range(0, 0, pos.line, pos.character));
+            var textUntilCursor = editor.document.getText(new Range(0, 0, pos.line, pos.character));
             cursorOffset.text = "Offset: " + Buffer.byteLength(textUntilCursor);
             cursorOffset.show();
         }
 
-        context.subscriptions.push(Vscode.window.onDidChangeTextEditorSelection(function(_) updateItem()));
-        context.subscriptions.push(Vscode.window.onDidChangeActiveTextEditor(function(_) updateItem()));
+        context.subscriptions.push(window.onDidChangeTextEditorSelection(function(_) updateItem()));
+        context.subscriptions.push(window.onDidChangeActiveTextEditor(function(_) updateItem()));
         updateItem();
     }
 
     @:expose("activate")
-    static function activate(context:vscode.ExtensionContext) {
+    static function activate(context:ExtensionContext) {
         new Main(context);
     }
 }
