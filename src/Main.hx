c19912a8d76f16517bd8a5f2b7860d774cd80f15
Gama11 <jensfischer95@gmail.com>
2017-02-28 15:03:52 +0100

    Add HxTestSeparatorFeature

--- src/Main.hx
+++ src/Main.hx
@@ -1,17 +1,15 @@
 package;
 
 import features.CursorOffsetFeature;
+import features.HxTestSeparatorFeature;
 import features.hxParserVis.HxParserVisFeature;
 import vscode.*;
 
 class Main {
-    function new(context:ExtensionContext) {
-        new HxParserVisFeature(context);
-        new CursorOffsetFeature(context);
-    }
-
     @:expose("activate")
     static function activate(context:ExtensionContext) {
-        new Main(context);
+        new HxParserVisFeature(context);
+        new CursorOffsetFeature(context);
+        new HxTestSeparatorFeature(context);
     }
 }
